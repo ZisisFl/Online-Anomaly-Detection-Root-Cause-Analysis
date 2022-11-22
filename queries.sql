3b50b469649a02c5da5936eff88522494571aeed
Zisis Flokas <zisis.flokas@libramli.ai>
2022-11-22 21:49:04 +0200

    query for web sales updated

--- queries.sql
+++ queries.sql
@@ -26,7 +26,7 @@ from store_sales, date_dim
 where store_sales.ss_sold_date_sk = date_dim.d_date_sk
 group by date_dim.d_date
 
--- min and max number of sales per day -> 560 4074
+-- min and max number of sales per day -> 560 4074 (70 1123 for web sales)
 select min(number_of_sales), max(number_of_sales)
 from (
 	select date_dim.d_date as sale_date, count(*) as number_of_sales
@@ -63,4 +63,27 @@ select store_sales.*,
 from store_sales, date_dim, time_dim
 where store_sales.ss_sold_date_sk = date_dim.d_date_sk
 and store_sales.ss_sold_time_sk = time_dim.t_time_sk
+limit 1000
+
+-- query for websales with some dimension
+select web_sales.ws_item_sk,
+web_sales.ws_order_number,
+item.i_brand_id,
+item.i_class_id,
+item.i_category_id,
+item.i_manufact_id,
+customer_address.ca_city,
+customer_address.ca_county,
+customer_address.ca_state,
+customer_address.ca_country,
+ship_mode.sm_type,
+ship_mode.sm_code,
+ship_mode.sm_carrier,
+to_timestamp(CONCAT(to_char(date_dim.d_date, 'YYYY-MM-DD'), ' ', time_dim.t_hour, ':', time_dim.t_minute, ':', time_dim.t_second), 'YYYY-MM-DD HH24:MI:SS') as sale_at
+from web_sales, date_dim, time_dim, item, customer_address, ship_mode
+where web_sales.ws_sold_date_sk = date_dim.d_date_sk
+and web_sales.ws_sold_time_sk = time_dim.t_time_sk
+and web_sales.ws_item_sk = item.i_item_sk
+and web_sales.ws_ship_addr_sk = customer_address.ca_address_sk
+and web_sales.ws_ship_mode_sk = ship_mode.sm_ship_mode_sk
 limit 1000
