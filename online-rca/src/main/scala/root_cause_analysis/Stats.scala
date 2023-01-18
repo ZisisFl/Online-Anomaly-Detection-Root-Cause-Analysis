0b4bfee86552ccade288a44fb97f6aba1ed3e9c3
Zisis Flokas <zhshs97@gmail.com>
2023-01-18 23:46:56 +0200

    rca stats more methods implemented

--- online-rca/src/main/scala/root_cause_analysis/Stats.scala
+++ online-rca/src/main/scala/root_cause_analysis/Stats.scala
@@ -2,10 +2,40 @@ package root_cause_analysis
 
 class Stats {
 
-  def computeValueChangePercentage(baseline: Double, current: Double): Unit = {
+  def computeValueChangePercentage(baseline: Double, current: Double): Double = {
     if (baseline != 0d) {
       val percentageChange = ((current - baseline) / baseline) * 100d
       roundUp(percentageChange)
+    }
+    else {
+      Double.NaN
+    }
+  }
+
+  def computeContributionChangePercentage(
+                                           baseline: Double,
+                                           current: Double,
+                                           baselineTotal: Double,
+                                           currentTotal: Double): Double = {
+
+    if (currentTotal != 0d && baselineTotal != 0d) {
+      val contributionChange = ((current / currentTotal) - (baseline / baselineTotal)) * 100d
+      roundUp(contributionChange)
+    }
+    else {
+      Double.NaN
+    }
+  }
+
+  def computeContributionToOverallChangePercentage(
+                                                    baseline: Double,
+                                                    current: Double,
+                                                    baselineTotal: Double,
+                                                    currentTotal: Double
+                                                  ): Double = {
+    if (baselineTotal != 0d) {
+      val contributionToOverallChange = ((current - baseline) / Math.abs(currentTotal - baselineTotal)) * 100d
+      roundUp(contributionToOverallChange)
     }
     else {
       Double.NaN
