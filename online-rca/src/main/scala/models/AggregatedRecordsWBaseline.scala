34227796ca02c713e3bf7368659507b8e7d82110
Zisis Flokas <zhshs97@gmail.com>
2023-02-05 13:25:30 +0200

    added AggregatedRecordsWBaseline to repo

---
+++ online-rca/src/main/scala/models/AggregatedRecordsWBaseline.scala
@@ -0,0 +1,11 @@
+package models
+
+import utils.Types.MetricValue
+
+case class AggregatedRecordsWBaseline(
+                                       current: MetricValue,
+                                       baseline: MetricValue,
+                                       current_dimensions_breakdown: Map[Dimension, MetricValue],
+                                       baseline_dimensions_breakdown: Map[Dimension, MetricValue],
+                                       records_in_offset: Int
+                                     )
