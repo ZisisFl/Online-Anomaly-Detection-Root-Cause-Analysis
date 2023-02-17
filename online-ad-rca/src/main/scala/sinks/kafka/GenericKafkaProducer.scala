273dc58a2c462e8f06117aefa78b04a225c79e81
Zisis Flokas <zhshs97@gmail.com>
2023-02-17 10:52:25 +0200

    JSON Kafka producer in progress

--- online-ad-rca/src/main/scala/sinks/kafka/GenericKafkaProducer.scala
+++ online-ad-rca/src/main/scala/sinks/kafka/GenericKafkaProducer.scala
@@ -1,8 +1,22 @@
 package sinks.kafka
 
 import config.AppConfig
+import models.RCAResult
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer
+import serialization.RCAResultSerializationSchema
 
-case class GenericKafkaProducer(topic: String) extends FlinkKafkaProducer[String](AppConfig.Kafka.BOOTSTRAP_SERVERS, topic, new SimpleStringSchema())
+import java.util.Properties
+
+case class GenericKafkaProducer(topic: String) extends FlinkKafkaProducer[RCAResult](
+  topic,
+  new RCAResultSerializationSchema(topic),
+  {
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", AppConfig.Kafka.BOOTSTRAP_SERVERS)
+
+    properties
+  },
+  FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+)
 //https://github.com/apache/flink/blob/db4f7781173bdc5790f7b3e1fd3eeebfa4b31fc9/flink-formats/flink-json/src/main/java/org/apache/flink/formats/json/JsonSerializationSchema.java
